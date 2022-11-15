064b8b057ecf55c0d9ab0274320bccd37ac2068e
junioranheu <junioranheu@gmail.com>
2022-11-15 17:38:26 -0300

    ajustes gerais

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, NgIterable, OnInit } from '@angular/core';
+import { Auth, UsuarioContext } from 'src/utils/context/usuarioContext';
 import iItem from 'src/utils/interfaces/item';
 import { ItemService } from 'src/utils/services/item.service';
 
@@ -9,10 +10,15 @@ import { ItemService } from 'src/utils/services/item.service';
 })
 export class IndexComponent implements OnInit {
 
-    constructor(private itemService: ItemService) { }
+    constructor(private usuarioContext: UsuarioContext, private itemService: ItemService) { }
 
+    isAuth: boolean | undefined;
+    nomeUsuarioLogado?: string;
     listaItens: iItem[] | null | undefined | NgIterable<any>;
     async ngOnInit(): Promise<void> {
+        this.usuarioContext.isAuthObservable.subscribe(ia => this.isAuth = ia);
+        this.nomeUsuarioLogado = (this.isAuth ? Auth.get()?.nomeUsuarioSistema : '');
+
         this.listaItens = await this.itemService.getListaItensGroupByUsuario();
         // console.log(this.listaItens);
     }
