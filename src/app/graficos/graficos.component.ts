0b3583fc2fa4cb5609c4bac7dcdde531d8c85deb
junioranheu <junioranheu@gmail.com>
2022-11-23 23:54:50 -0300

    ajustes gerais referentes aos charts e tabs

--- src/app/graficos/graficos.component.ts
+++ src/app/graficos/graficos.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 import { AgChartOptions } from 'ag-charts-community';
+import iItemAgrupadoByItemTipo, { propriedadesIItemAgrupadoByItemTipo } from 'src/utils/interfaces/itemAgrupadoByItemTipo';
 import { getAgChartsAngularModuleDataMock, propriedadesAgChartsAngularModuleDataMock } from 'src/utils/mock/agChartsAngularModuleDataMock';
+import { ItemService } from 'src/utils/services/item.service';
 
 // Tutorial: https://www.ag-grid.com/angular-charts/gallery/simple-bar/
 @Component({
@@ -11,9 +13,9 @@ import { getAgChartsAngularModuleDataMock, propriedadesAgChartsAngularModuleData
 })
 export class GraficosComponent implements OnInit {
 
-    dadosChart: AgChartOptions;
-    constructor() {
-        this.dadosChart = {
+    dadosChartMockado: AgChartOptions;
+    constructor(private itemService: ItemService) {
+        this.dadosChartMockado = {
             autoSize: true,
             data: getAgChartsAngularModuleDataMock(),
             theme: {
@@ -21,13 +23,14 @@ export class GraficosComponent implements OnInit {
                     bar: {
                         series: {
                             strokeWidth: 0,
+                            fill: 'rgba(154, 107, 255, 1)'
                         },
                     },
                 },
             },
             title: {
                 text: 'Gráfico com dados mockados',
-                fontSize: 18,
+                fontSize: 20,
             },
             subtitle: {
                 text: 'Isso é apenas um teste de implementação de charts com Angular',
@@ -59,7 +62,58 @@ export class GraficosComponent implements OnInit {
         };
     }
 
-    ngOnInit(): void {
+    dadosChartAPI: AgChartOptions | undefined;
+    async ngOnInit(): Promise<void> {
+        const listaItens = await this.itemService.getListaItensGroupByItemTipo() as iItemAgrupadoByItemTipo[];
+        // console.log(listaItens);
+
+        if (listaItens) {
+            this.dadosChartAPI = {
+                autoSize: true,
+                data: listaItens,
+                theme: {
+                    overrides: {
+                        bar: {
+                            series: {
+                                strokeWidth: 0,
+                                fill: 'rgba(154, 107, 255, 1)',
+                            },
+                        },
+                    },
+                },
+                title: {
+                    text: 'Gráfico com dados da API',
+                    fontSize: 20,
+                },
+                subtitle: {
+                    text: 'Isso é apenas um teste de implementação de charts com Angular',
+                },
+                series: [
+                    {
+                        type: 'bar',
+                        xKey: propriedadesIItemAgrupadoByItemTipo.xItemTipo,
+                        yKey: propriedadesIItemAgrupadoByItemTipo.xQuantidade,
+                    },
+                ],
+                axes: [
+                    {
+                        type: 'category',
+                        position: 'left',
+                    },
+                    {
+                        type: 'number',
+                        position: 'bottom',
+                        title: {
+                            enabled: true,
+                            text: 'Quantidade de itens',
+                        },
+                    },
+                ],
+                legend: {
+                    enabled: true,
+                },
+            };
+        }
     }
 
 }
---
+++ src/utils/interfaces/itemAgrupadoByItemTipo.ts
@@ -0,0 +1,10 @@
+export default interface iItemAgrupadoByItemTipo {
+    xItemTipo: string;
+    xItemTipoId: number;
+    xQuantidade: number;
+}
+
+export const propriedadesIItemAgrupadoByItemTipo = {
+    xItemTipo: 'xItemTipo',
+    xQuantidade: 'xQuantidade'
+};
